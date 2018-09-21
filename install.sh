0777801e30c443fafa1f3ade423e52933300993c
JasonCoombs <ceo@forensics.org>
2018-09-20 16:20:44 -1000

    Update install.sh for local re-build

--- install.sh
+++ install.sh
@@ -753,17 +753,21 @@ build_from_travis()
 #==============================================================================
 build_all()
 {
+if [ ! -d "$BUILD_DIR/libbitcoin-server" ]; then
     build_from_tarball_boost $BOOST_URL $BOOST_ARCHIVE bzip2 . $PARALLEL "$BUILD_BOOST" "${BOOST_OPTIONS[@]}"
     build_from_tarball $ZMQ_URL $ZMQ_ARCHIVE gzip . $PARALLEL "$BUILD_ZMQ" "${ZMQ_OPTIONS[@]}" "$@"
     build_from_github libbitcoin secp256k1 version5 $PARALLEL ${SECP256K1_OPTIONS[@]} "$@"
-    build_from_github libbitcoin libbitcoin master $PARALLEL ${BITCOIN_OPTIONS[@]} "$@"
-    build_from_github libbitcoin libbitcoin-consensus master $PARALLEL ${BITCOIN_CONSENSUS_OPTIONS[@]} "$@"
-    build_from_github libbitcoin libbitcoin-database master $PARALLEL ${BITCOIN_DATABASE_OPTIONS[@]} "$@"
-    build_from_github libbitcoin libbitcoin-blockchain master $PARALLEL ${BITCOIN_BLOCKCHAIN_OPTIONS[@]} "$@"
-    build_from_github libbitcoin libbitcoin-network master $PARALLEL ${BITCOIN_NETWORK_OPTIONS[@]} "$@"
-    build_from_github libbitcoin libbitcoin-node master $PARALLEL ${BITCOIN_NODE_OPTIONS[@]} "$@"
-    build_from_github libbitcoin libbitcoin-protocol master $PARALLEL ${BITCOIN_PROTOCOL_OPTIONS[@]} "$@"
-    build_from_travis libbitcoin libbitcoin-server master $PARALLEL ${BITCOIN_SERVER_OPTIONS[@]} "$@"
+    build_from_github JasonCoombs libbitcoin master $PARALLEL ${BITCOIN_OPTIONS[@]} "$@"
+    build_from_github JasonCoombs libbitcoin-consensus master $PARALLEL ${BITCOIN_CONSENSUS_OPTIONS[@]} "$@"
+    build_from_github JasonCoombs libbitcoin-database master $PARALLEL ${BITCOIN_DATABASE_OPTIONS[@]} "$@"
+    build_from_github JasonCoombs libbitcoin-blockchain master $PARALLEL ${BITCOIN_BLOCKCHAIN_OPTIONS[@]} "$@"
+    build_from_github JasonCoombs libbitcoin-network master $PARALLEL ${BITCOIN_NETWORK_OPTIONS[@]} "$@"
+    build_from_github JasonCoombs libbitcoin-node master $PARALLEL ${BITCOIN_NODE_OPTIONS[@]} "$@"
+    build_from_github JasonCoombs libbitcoin-protocol master $PARALLEL ${BITCOIN_PROTOCOL_OPTIONS[@]} "$@"
+    build_from_travis JasonCoombs libbitcoin-server master $PARALLEL ${BITCOIN_SERVER_OPTIONS[@]} "$@"
+else
+    build_from_local "local re-build" $PARALLEL ${BITCOIN_OPTIONS[@]} "$@"
+fi
 }
 
 
@@ -773,9 +777,11 @@ if [[ $DISPLAY_HELP ]]; then
     display_help
 else
     display_configuration
+if [ ! -d "$BUILD_DIR" ]; then
     create_directory "$BUILD_DIR"
     push_directory "$BUILD_DIR"
     initialize_git
     pop_directory
+fi
     time build_all "${CONFIGURE_OPTIONS[@]}"
 fi
